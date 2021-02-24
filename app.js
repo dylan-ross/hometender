3d15cacd08611d59effd018b24d17ec36b804d88
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-24 13:00:31 -0500

    finished psuedo code, async function fetchs data

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 // Psuedo code
 // add API endpoints
+// console.log("lets get drunk")
+
+// const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+
+
+//get cocktail Data
+
+async function getData(cocktail) {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+  try {
+    const response = await axios.get(url)
+    let data = response.data
+    console.log(data)
+    return response
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// getData('old fashion')
+
+
+
+// create tags and set src append to DOM
 
 
 
 
 
 
-// request image by cocktail
-
-
-
-
-
-// request recipe by cocktail
-
-
-
-
-
-// request glassware by cocktail
-
-
-
-
-// create img tag and set src append to DOM
-
-
-
-
-
-
-// create tag for recipe and glassware set src append to DOM
 
 
 
