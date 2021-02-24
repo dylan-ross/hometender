fd06e4f17959865bf1a72ea0fe326eadf31696fd
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-24 11:18:08 -0500

    psuedo code

---
+++ app.js
@@ -0,0 +1,82 @@
+// Psuedo code
+// add API endpoints
+
+
+
+
+
+
+// request image by cocktail
+
+
+
+
+
+// request recipe by cocktail
+
+
+
+
+
+// request glassware by cocktail
+
+
+
+
+// create img tag and set src append to DOM
+
+
+
+
+
+
+// create tag for recipe and glassware set src append to DOM
+
+
+
+
+
+// remove previous cocktails
+
+
+
+
+
+
+
+// create effect eventListener to reveal recipe
+
+
+
+
+
+
+// request cocktails by ingredient
+
+
+
+
+
+// create tag for cocktails set src append to DOM
+
+
+
+
+// link image/recipe reveal element to cocktail names
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
