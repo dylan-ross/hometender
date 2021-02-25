8aa881442b77e2175289e84585b27ce3aa8fb45a
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-25 18:16:11 -0500

    working on responsive design

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 // Psuedo code
 // add API endpoints
 // console.log("lets get drunk")
-
-// const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
-
-
 //get cocktail Data
 
 async function getData(cocktail) {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
-  // const headers = {
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*'
-  //   }
-  // }
   try {
     let response = await axios.get(url)
     let data = response.data
-    // console.log(data.drinks[0].strDrinkThumb)
     showCocktailImage(data)
     showCocktailData(data)
     return response
@@ -26,27 +16,17 @@ async function getData(cocktail) {
   }
 } 
 
-getData('manhattan')
-
-
-
-// // create tags and set src append to DOM
-
 function showCocktailImage(data) {
-  console.log(data.drinks[0].strDrinkThumb)
   const imageContainer = document.querySelector('#front')
-  // console.log(imageContainer)
   const cocktailImage = `
     <h5 class="name">${data.drinks[0].strDrink}</h5>
     <img src="${data.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>
 `
-  console.log(cocktailImage)
-  imageContainer.insertAdjacentHTML('beforeend', cocktailImage)
+  imageContainer.insertAdjacentHTML('afterbegin', cocktailImage)
 }
 
 function showCocktailData(data) {
   const dataContainer = document.querySelector('#back')
-  console.log(dataContainer)
   const cocktailData = `
     <h5 class="back name">${data.drinks[0].strDrink}</h5>
     <p class="back ing-msr">${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
@@ -57,51 +37,24 @@ function showCocktailData(data) {
     <p class="back instruction">${data.drinks[0].strInstructions}</p>
     <p class="back glassware">${data.drinks[0].strGlass}</p>
   `
-  dataContainer.insertAdjacentHTML('beforeend', cocktailData )
+  dataContainer.insertAdjacentHTML('afterbegin', cocktailData )
 }
 
-
-// addEventListener to take in search input
-
-const form = document.querySelector('form')
-form.addEventListener('button', (e) => {
+const search = document.querySelector('form')
+search.addEventListener('submit', (e) => {
   e.preventDefault()
   const inputValue = document.querySelector('#cts-name').value
-  // console.log(inputValue)
-  // removeFront()
+  removeFront()
   getData(inputValue)
   document.querySelector('#cts-name').value = ""
 })
 
-
-// console.log(form)
-
-
-
-
-
-
-// remove previous cocktails
-
-// function removeFront() {
-//   const frontContainer = document.querySelector('#front')
-//   // console.log(cocktailContainer)
-//   while (('.name') > 3) {
-//     frontContainer.removeChild(frontContainer.firstChild)
-//   }
-// }
-
-// removeFront()
-
-
-
-
-
-// create effect eventListener to reveal recipe
-
-
-
-
+function removeFront() {
+  const image = document.querySelector('#front')
+  while(image.childElementCount > 4) {
+    image.removeChild(image.lastChild)
+  }
+}
 
 
 // request cocktails by ingredient
