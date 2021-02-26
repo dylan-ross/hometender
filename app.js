b2d82b994b97c00333f2ef42380da82d39e53fe2
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-26 09:55:27 -0500

    flip card divs

--- app.js
+++ app.js
@@ -3,41 +3,55 @@
 // console.log("lets get drunk")
 //get cocktail Data
 
-async function getData(cocktail) {
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+async function getCocktailData(cocktail) {
+  const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
   try {
-    let response = await axios.get(url)
-    let data = response.data
-    showCocktailImage(data)
-    showCocktailData(data)
+    let response = await axios.get(cocktailUrl)
+    let ctData = response.ctData
+    showCocktailImage(ctData)
+    showCocktailData(ctData)
     return response
   } catch (err) {
     console.error(err)
   }
 } 
 
-function showCocktailImage(data) {
-  const imageContainer = document.querySelector('#front')
-  const cocktailImage = `
-    <h5 class="name">${data.drinks[0].strDrink}</h5>
-    <img src="${data.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>
-`
-  imageContainer.insertAdjacentHTML('afterbegin', cocktailImage)
+async function getCocktailList(ingredient) {
+  const ingredientUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin`
+  try {
+    let 
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-function showCocktailData(data) {
-  const dataContainer = document.querySelector('#back')
+function showCocktailImage(ctData) {
+  const card = document.querySelector('#front')
+  const imageBox = document.createElement('div')
+  imageBox.setAttribute("class", "face")
+  let cocktailImage = `
+    <h5 class="name">${ctData.drinks[0].strDrink}</h5>
+    <img src="${ctData.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>
+`  
+  imageBox.innerHTML = cocktailImage
+  card.appendChild(imageBox)
+}
+
+function showCocktailData(ctData) {
+  const back = document.createElement('div')
+  dataBox.setAttribute("class", "flip")
   const cocktailData = `
-    <h5 class="back name">${data.drinks[0].strDrink}</h5>
-    <p class="back ing-msr">${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
-    <p class="back ing-msr">${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}</p>
-    <p class="back ing-msr">${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}</p>
-    <p class="back ing-msr">${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}</p>
-    <p class="back ing-msr">${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}</p>
-    <p class="back instruction">${data.drinks[0].strInstructions}</p>
-    <p class="back glassware">${data.drinks[0].strGlass}</p>
+    <h5 class="back name">${ctData.drinks[0].strDrink}</h5>
+    <p class="back ing-msr">${ctData.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
+    <p class="back ing-msr">${ctData.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}</p>
+    <p class="back ing-msr">${ctData.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}</p>
+    <p class="back ing-msr">${ctData.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}</p>
+    <p class="back ing-msr">${ctData.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}</p>
+    <p class="back instruction">${ctData.drinks[0].strInstructions}</p>
+    <p class="back glassware">${ctData.drinks[0].strGlass}</p>
   `
-  dataContainer.insertAdjacentHTML('afterbegin', cocktailData )
+  back.innerHTML = cocktailData
+  card.appendChild(back)
 }
 
 const search = document.querySelector('form')
