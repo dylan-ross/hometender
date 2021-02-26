bacbf7c5c35cefe59614817547cbf871ff9e19c9
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-26 15:38:43 -0500

    removed keys with value of null from json object

--- app.js
+++ app.js
@@ -3,19 +3,47 @@
 // console.log("lets get drunk")
 //get cocktail Data
 
+// const myObj = { 
+//   b:'my object',
+//   c: '',
+//   d: null,
+//   f: {v: 1, x: '', y: null, m: {a:'asd'}}
+// };
+// const removeEmptyOrNull = (obj) => {
+//   Object.keys(obj).forEach(k =>
+//     (obj[k] && typeof obj[k] === 'object') && removeEmptyOrNull(obj[k]) ||
+//     (!obj[k] && obj[k] !== undefined) && delete obj[k]
+//   );
+//   return obj;
+// };
+// const myObj2 = removeEmptyOrNull(myObj);
+
+
+
+
+
+
+
 async function getCocktailData(cocktail) {
   const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
   try {
     let response = await axios.get(cocktailUrl)
     let data = response.data
-    cocktailCard(data)
-    // showCocktailData(data)
+    const removeEmptyOrNull = (obj) => {
+      Object.keys(obj).forEach(k =>
+        (obj[k] && typeof obj[k] === 'object') && removeEmptyOrNull(obj[k]) || 
+        (!obj[k] && obj[k] !== undefined) && delete obj[k])
+      return obj
+    }
+    const data2 = removeEmptyOrNull(data)
+    // console.log(data2)
+    cocktailCard(data2)
     return response
   } catch (err) {
     console.error(err)
   }
 } 
-
+getCocktailData('sazerac')
 // async function getCocktailList(ingredient) {
 //   const ingredientUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=>${ingredient}`
 //   try {
@@ -26,7 +54,7 @@ async function getCocktailData(cocktail) {
 //   }
 // }
 
-function cocktailCard(data) {
+function cocktailCard(data2) {
   const cardContainer = document.querySelector('.results')
   const card = document.createElement('div')
   const innerCard = document.createElement('div')
@@ -36,27 +64,28 @@ function cocktailCard(data) {
   innerCard.setAttribute('class', 'inner-card')
   frontCard.setAttribute('class', 'front-card')
   backCard.setAttribute('class', 'back-card')
-  const cocktailName = `<h5 class="name">${data.drinks[0].strDrink}</h5>`
-  const cocktailImage = `<img src="${data.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>`  
+  const cocktailName = `<h5 class="name">${data2.drinks[0].strDrink}</h5>`
+  const cocktailImage = `<img src="${data2.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>`
   const cocktailData = `
-  <p class="back ing-msr">${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure6} ${data.drinks[0].strIngredient6}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure7} ${data.drinks[0].strIngredient7}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure8} ${data.drinks[0].strIngredient8}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure9} ${data.drinks[0].strIngredient9}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure10} ${data.drinks[0].strIngredient10}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure11} ${data.drinks[0].strIngredient11}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure12} ${data.drinks[0].strIngredient12}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure13} ${data.drinks[0].strIngredient13}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure14} ${data.drinks[0].strIngredient14}</p>
-  <p class="back ing-msr">${data.drinks[0].strMeasure15} ${data.drinks[0].strIngredient15}</p>
-  <p class="back instruction">${data.drinks[0].strInstructions}</p>
-  <p class="back glassware">${data.drinks[0].strGlass}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure1} ${data2.drinks[0].strIngredient1} </p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure2} ${data2.drinks[0].strIngredient2}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure3} ${data2.drinks[0].strIngredient3}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure4} ${data2.drinks[0].strIngredient4}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure5} ${data2.drinks[0].strIngredient5}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure6} ${data2.drinks[0].strIngredient6}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure7} ${data2.drinks[0].strIngredient7}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure8} ${data2.drinks[0].strIngredient8}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure9} ${data2.drinks[0].strIngredient9}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure10} ${data2.drinks[0].strIngredient10}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure11} ${data2.drinks[0].strIngredient11}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure12} ${data2.drinks[0].strIngredient12}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure13} ${data2.drinks[0].strIngredient13}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure14} ${data2.drinks[0].strIngredient14}</p>
+  <p class="back ing-msr">${data2.drinks[0].strMeasure15} ${data2.drinks[0].strIngredient15}</p>
+  <p class="back instruction">${data2.drinks[0].strInstructions}</p>
+  <p class="back glassware">${data2.drinks[0].strGlass}</p>
   `
+  // const cocktailDataShrink  = cocktailData.value != null
   frontCard.innerHTML = cocktailImage
   backCard.innerHTML = cocktailData
   
@@ -77,8 +106,8 @@ search.addEventListener('submit', (e) => {
 
 function removeCard() {
   const results = document.querySelector('.results')
-  while(results.childElementCount > 4) {
-    results.removeChild(results.lastChild)
+  while(results.childElementCount > 3) {
+    results.removeChild(results.firstChild)
   }
 }
 
