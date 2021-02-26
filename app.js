ad8e9f2855a54725b912522ea19d3f882bfaa21f
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-02-26 13:41:41 -0500

    consolidated into cocktailCard function

--- app.js
+++ app.js
@@ -7,66 +7,78 @@ async function getCocktailData(cocktail) {
   const cocktailUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
   try {
     let response = await axios.get(cocktailUrl)
-    let ctData = response.ctData
-    showCocktailImage(ctData)
-    showCocktailData(ctData)
+    let data = response.data
+    cocktailCard(data)
+    // showCocktailData(data)
     return response
   } catch (err) {
     console.error(err)
   }
 } 
 
-async function getCocktailList(ingredient) {
-  const ingredientUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin`
-  try {
-    let 
-  } catch (err) {
-    console.error(err)
-  }
-}
+// async function getCocktailList(ingredient) {
+//   const ingredientUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=>${ingredient}`
+//   try {
+//     let ingResponse = await axios.get(ingredientUrl)
+//     let ingData = ingResponse.
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
 
-function showCocktailImage(ctData) {
-  const card = document.querySelector('#front')
-  const imageBox = document.createElement('div')
-  imageBox.setAttribute("class", "face")
-  let cocktailImage = `
-    <h5 class="name">${ctData.drinks[0].strDrink}</h5>
-    <img src="${ctData.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>
-`  
-  imageBox.innerHTML = cocktailImage
-  card.appendChild(imageBox)
-}
-
-function showCocktailData(ctData) {
-  const back = document.createElement('div')
-  dataBox.setAttribute("class", "flip")
+function cocktailCard(data) {
+  const cardContainer = document.querySelector('.results')
+  const card = document.createElement('div')
+  const innerCard = document.createElement('div')
+  const frontCard = document.createElement('div')
+  const backCard = document.createElement('div')
+  card.setAttribute('class', 'card')
+  innerCard.setAttribute('class', 'inner-card')
+  frontCard.setAttribute('class', 'front-card')
+  backCard.setAttribute('class', 'back-card')
+  const cocktailName = `<h5 class="name">${data.drinks[0].strDrink}</h5>`
+  const cocktailImage = `<img src="${data.drinks[0].strDrinkThumb}" crossorigin="anonymous" alt="cocktail" class="image"/>`  
   const cocktailData = `
-    <h5 class="back name">${ctData.drinks[0].strDrink}</h5>
-    <p class="back ing-msr">${ctData.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
-    <p class="back ing-msr">${ctData.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}</p>
-    <p class="back ing-msr">${ctData.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}</p>
-    <p class="back ing-msr">${ctData.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}</p>
-    <p class="back ing-msr">${ctData.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}</p>
-    <p class="back instruction">${ctData.drinks[0].strInstructions}</p>
-    <p class="back glassware">${ctData.drinks[0].strGlass}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1} </p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure6} ${data.drinks[0].strIngredient6}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure7} ${data.drinks[0].strIngredient7}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure8} ${data.drinks[0].strIngredient8}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure9} ${data.drinks[0].strIngredient9}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure10} ${data.drinks[0].strIngredient10}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure11} ${data.drinks[0].strIngredient11}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure12} ${data.drinks[0].strIngredient12}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure13} ${data.drinks[0].strIngredient13}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure14} ${data.drinks[0].strIngredient14}</p>
+  <p class="back ing-msr">${data.drinks[0].strMeasure15} ${data.drinks[0].strIngredient15}</p>
+  <p class="back instruction">${data.drinks[0].strInstructions}</p>
+  <p class="back glassware">${data.drinks[0].strGlass}</p>
   `
-  back.innerHTML = cocktailData
-  card.appendChild(back)
+  frontCard.innerHTML = cocktailImage
+  backCard.innerHTML = cocktailData
+  
+  card.appendChild(innerCard)
+  innerCard.appendChild(frontCard)
+  innerCard.append(backCard)
+  cardContainer.appendChild(card)
 }
 
 const search = document.querySelector('form')
 search.addEventListener('submit', (e) => {
   e.preventDefault()
   const inputValue = document.querySelector('#cts-name').value
-  removeFront()
-  getData(inputValue)
+  removeCard()
+  getCocktailData(inputValue)
   document.querySelector('#cts-name').value = ""
 })
 
-function removeFront() {
-  const image = document.querySelector('#front')
-  while(image.childElementCount > 4) {
-    image.removeChild(image.lastChild)
+function removeCard() {
+  const results = document.querySelector('.results')
+  while(results.childElementCount > 4) {
+    results.removeChild(results.lastChild)
   }
 }
 
